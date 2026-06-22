62a95b9b56ee50c132a7a30436dcd93dee2b937a
Manoj Mamidi <manoj@example.com>
2026-06-22 15:09:32 +0530

    login error

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,14 @@ import { errorHandler, notFoundHandler } from './middleware/error.middleware.js'
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || import.meta.env.VITE_API_URL}));
+//app.use(cors({ origin: process.env.CLIENT_URL || import.meta.env.VITE_API_URL}));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api', routes);
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
